8121b36a33a37640034f454e03792f115b4c5d7e
fvdb <frank@am-knie.de>
2014-12-08 20:42:43 +0100

    add pong

--- sfml/pong/1.cpp
+++ sfml/pong/1.cpp
@@ -1,26 +1,36 @@
-#include <SFML/Window.hpp>
+#ifdef SFML_STATIC
+#pragma comment(lib, "glew.lib")
+#pragma comment(lib, "freetype.lib")
+#pragma comment(lib, "jpeg.lib")
+#pragma comment(lib, "opengl32.lib")
+#pragma comment(lib, "winmm.lib")
+#pragma comment(lib, "gdi32.lib")  
+#endif // SFML_STATIC
+
+
 #include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
 
 using namespace std;
 using namespace sf;
 
-constexpr int windowWidth{800}, windowHeigth{600};
+unsigned int windowWidth{ 800 }, windowHeigth{ 600 };
 
 int main()
 {
-    // Erzeugung game window
-    RenderWindow window{{windowWidth, windowHeigth}, "Single Player Pong"};
-    window.setFramerateLimit(60);
+	// Erzeugung game window
+	RenderWindow window{ { windowWidth, windowHeigth }, "Single Player Pong" };
+	window.setFramerateLimit(60);
 
-    while(true)
-    {
-        window.clear(Color::Black);
+	while (true)
+	{
+		window.clear(Color::Black);
 
-        if(Keyboard::isKeyPressed(Keyboard::Key::Escape)) break;
+		if (Keyboard::isKeyPressed(Keyboard::Key::Escape)) break;
 
-        window.display();
-    }
+		window.display();
+	}
 }
 
 
-    
+
--- sfml/pong/2.cpp
+++ sfml/pong/2.cpp
@@ -1,44 +1,55 @@
-#include <SFML/Window.hpp>
+#ifdef SFML_STATIC
+#pragma comment(lib, "glew.lib")
+#pragma comment(lib, "freetype.lib")
+#pragma comment(lib, "jpeg.lib")
+#pragma comment(lib, "opengl32.lib")
+#pragma comment(lib, "winmm.lib")
+#pragma comment(lib, "gdi32.lib")  
+#endif // SFML_STATIC
+
+
 #include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+
 
 using namespace std;
 using namespace sf;
 
-constexpr int windowWidth{800}, windowHeigth{600};
-constexpr float ballRadius{10.f};
+unsigned int windowWidth{ 800 }, windowHeigth{ 600 };
+float ballRadius{ 10.f };
 
 struct Ball {
-    // CircleShape ist eine SFML Klasse die
-    // renderbare Kreise definiert
-    CircleShape shape;
+	// CircleShape ist eine SFML Klasse die
+	// renderbare Kreise definiert
+	CircleShape shape;
 
-    // Konstruktor
-    Ball(float mX, float mY) {
-        shape.setPosition(mX, mY);
-        shape.setRadius(ballRadius);
-        shape.setFillColor(Color::White);
-        shape.setOrigin(ballRadius, ballRadius);
-    }
+	// Konstruktor
+	Ball(float mX, float mY) {
+		shape.setPosition(mX, mY);
+		shape.setRadius(ballRadius);
+		shape.setFillColor(Color::White);
+		shape.setOrigin(ballRadius, ballRadius);
+	}
 };
 
 
 int main() {
-    // Instanz der Klasse Ball
-    Ball ball{windowWidth / 2, windowHeigth / 2};
+	// Instanz der Klasse Ball
+	Ball ball{ float(windowWidth) / 2, float(windowHeigth) / 2 };
 
-    // Erzeugung game window
-    RenderWindow window{{windowWidth, windowHeigth}, "Single Player Pong"};
-    window.setFramerateLimit(60);
+	// Erzeugung game window
+	RenderWindow window{ { windowWidth, windowHeigth }, "Single Player Pong" };
+	window.setFramerateLimit(60);
 
-    while(true)
-    {
-        window.clear(Color::Black);
+	while (true)
+	{
+		window.clear(Color::Black);
 
-        if(Keyboard::isKeyPressed(Keyboard::Key::Escape)) break;
-        window.draw(ball.shape);
-        window.display();
-    }
+		if (Keyboard::isKeyPressed(Keyboard::Key::Escape)) break;
+		window.draw(ball.shape);
+		window.display();
+	}
 }
 
 
-    
+
